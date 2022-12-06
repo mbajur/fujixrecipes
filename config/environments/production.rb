992a6ef6e463d3229d6b31cff5879f4372da80c0
mbajur <mbajur@gmail.com>
2022-12-06 09:26:59 +0100

    Change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {
-    host: ENV.fetch('HOST', 'localhost:3000'),
+    host: ENV.fetch('HOST', 'fujixweekly.com'),
     protocol: :https
   }
 
